c7842dfb45ef20a67b260c1ff19843272218efdf
F-19-F <1767306012@qq.com>
2020-01-30 16:44:41 +0800

    利用栈实现的数制转换

---
+++ 数据结构篇/链表/实战篇/进制转换/Bin2Dec.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<cstdio>
+#include"Stack.h"
+using namespace std;
+int bin2dec(char* Source)
+{
+    Stack a;
+    int Base_2=1;
+    int Temp;
+    int Result=0;
+    while (*Source!='\0')
+    {
+        Temp=(int)(*Source)-48;
+        a.Push(&Temp);
+        Source++;
+    }
+    while(a.Pop(&Temp))
+    {
+        Result+=Base_2*Temp;
+        Base_2*=2;
+    }
+    return Result;
+}
+int main()
+{
+    char a[99];
+    cin>>a;
+    cout<<bin2dec(a);
+    return 0;
+}
