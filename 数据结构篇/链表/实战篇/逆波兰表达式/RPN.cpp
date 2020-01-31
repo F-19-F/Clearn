50b9ce7dcecf0654cc6e8cbb91b05c8ef18d4a7d
F-19-F <1767306012@qq.com>
2020-01-31 15:58:21 +0800

    add support for float and fix some problems

--- 数据结构篇/链表/实战篇/逆波兰表达式/RPN.cpp
+++ 数据结构篇/链表/实战篇/逆波兰表达式/RPN.cpp
@@ -1,10 +1,4 @@
-#define Data_Type int
-#define char2int(Source) (int)(Source - 48)
-#define Get_Opt                                 \
-    for (int i = 0; i < 2; i++)                 \
-    {                                           \
-        Exp_Temp.Pop(&Temp), Opt_Num[i] = Temp; \
-    }
+#define Data_Type double
 
 #include <cstdlib>
 #include "Stack.h"
@@ -12,84 +6,86 @@
 
 using namespace std;
 
-int RPN(char *Expression)
+double RPN(char *Expression)
 {
-    Stack Num_Temp;
+    int Num_i = 0; //用来判断下面Num_Temp数组的下标
+    char Num_Temp[99];
     Stack Exp_Temp;
-    bool Opt_Signal=false;//为了避免两个连续的运算符导致多次入栈
-    int Temp; //万能临时变量
-    int Num_Tp;
-    int Num_Base = 1;
-    int Opt_Num[2];
+    bool Opt_Signal = false; //为了避免两个连续的运算符导致多次入栈
+    double Temp;             //万能临时变量
+    double Num_Tp;
+    double Opt_Num[2];
     while (*Expression != '\0')
     {
         //将表达式中的数字型字符存储在数字缓冲栈中
-        if (*Expression >= '0' && *Expression <= '9')
+        if ((*Expression >= '0' && *Expression <= '9') || *Expression == '.')
         {
-            Temp = char2int(*Expression);
-            Num_Temp.Push(&Temp);
-            Opt_Signal=false;
+            Num_Temp[Num_i++] = *Expression;
+            Num_Temp[Num_i] = '\0';
+            Opt_Signal = false;
         }
-        //接收到非数字字符时将数字缓冲中的数据转化成int型
-        if ((*Expression>'9'||*Expression<'0')&&!Opt_Signal)
+        //接收到非数字相关字符时将数字缓冲中的数据转化成double
+        if ((*Expression > '9' || *Expression < '0') && (!Opt_Signal && *Expression != '.'))
         {
-            Num_Tp = 0;
-            //将在数字缓冲栈中的数据转化为int数据存放在Num_Tp中
-            while (Num_Temp.Pop(&Temp))
-            {
-                Num_Tp += Num_Base * Temp;
-                Num_Base *= 10;
-            }
-            //将Num_Tp中的数据压入表达式栈中并重置Num_Base
-            Num_Base = 1;
+            Num_Tp = atof(Num_Temp);
             Exp_Temp.Push(&Num_Tp);
+            Num_i = 0;
         }
         //当扫描到运算符时进行判断计算
-        if (*Expression == '+')
+        if (*Expression == '+' || *Expression == '-' || *Expression == '*' || *Expression == '/')
         {
-            Get_Opt; //解决栈的先进后处原则对运算顺序的干扰
-            Temp = Opt_Num[1] + Opt_Num[0];
-            Exp_Temp.Push(&Temp);
-            Opt_Signal=true;
+            for (int i = 0; i < 2; i++)
+            {
+                if(Exp_Temp.Pop(&Temp))
+                Opt_Num[i] = Temp;
+                else//没有两个数字在数字栈中则表达式有误
+                {
+                     cout<<"表达式错误!"<<endl;
+                     return -1;
+                }
+                
+            }
+            Opt_Signal = true;
+            switch (*Expression)
+            {
+            case '+':
+                Temp = Opt_Num[1] + Opt_Num[0];
+                Exp_Temp.Push(&Temp);
+                break;
+            case '-':
+                Temp = Opt_Num[1] - Opt_Num[0];
+                Exp_Temp.Push(&Temp);
+                break;
+            case '*':
+                Temp = Opt_Num[1] * Opt_Num[0];
+                Exp_Temp.Push(&Temp);
+                break;
+            case '/':
+                if (Opt_Num[0] == 0)
+                {
+                    cout << "除数不能为0" << endl;
+                    return -2;
+                }
+                Temp = Opt_Num[1] / Opt_Num[0];
+                Exp_Temp.Push(&Temp);
+                break;
+            }
         }
-        if (*Expression == '-')
-        {
-            Get_Opt; //解决栈的先进后处原则对运算顺序的干扰
-            Temp = Opt_Num[1] - Opt_Num[0];
-            Exp_Temp.Push(&Temp);
-            Opt_Signal=true;
-        }
-        if (*Expression == '*')
-        {
-            Get_Opt; //解决栈的先进后处原则对运算顺序的干扰
-            Temp = Opt_Num[1] * Opt_Num[0];
-            Exp_Temp.Push(&Temp);
-            Opt_Signal=true;
-        }
-        if (*Expression == '/')
-        {
-            Get_Opt; //解决栈的先进后处原则对运算顺序的干扰
-            Temp = Opt_Num[1] / Opt_Num[0];
-            Exp_Temp.Push(&Temp);
-            Opt_Signal=true;
-        }
-        //将指针指向表达式的下一个字符
         Expression++;
     }
     Exp_Temp.Pop(&Temp);
-    if (Exp_Temp.Getlen()==0)
-    return Temp;
+    if (Exp_Temp.Getlen() == 0)
+        return Temp;
     else
     {
-        cout<<"表达式错误,结果不可靠"<<endl;
-        return 0;
+        cout << "表达式错误!" << endl;
+        return -3;
     }
-    
 }
 int main()
 {
     char Exp[100];
-    cin.get(Exp,100);
-    cout<<RPN(Exp);
+    cin.get(Exp, 100);
+    cout << RPN(Exp);
     return 0;
 }
